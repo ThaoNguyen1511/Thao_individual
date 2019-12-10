3067962a19ea371e3c0314a79ab8802851474811
Nguyen,Thao T.T.T <ttt.nguyen@student.fontys.nl>
2019-12-10 21:30:54 +0100

    DOCS created and gradle updated

--- src/test/java/UserServiceImplTest.java
+++ src/test/java/UserServiceImplTest.java
@@ -1,9 +1,15 @@
+import UserService.Client;
+import UserService.Message;
 import UserService.UserServiceImpl;
+import static org.mockito.Mockito.*;
+
 import org.junit.Test;
 
 public class UserServiceImplTest {
     @Test
     public void userShouldGetNewPassword(){
         UserServiceImpl userServiceImpl = new UserServiceImpl();
+        Client client = mock(Client.class);
+        Message message = mock(Message.class);
     }
 }
