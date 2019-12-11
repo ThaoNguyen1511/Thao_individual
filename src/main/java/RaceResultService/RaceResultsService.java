f2ed3815842b4d7a65505e0477a2d63f29b851de
Nguyen,Thao T.T.T <ttt.nguyen@student.fontys.nl>
2019-12-11 12:09:10 +0100

    Creation SUT

---
+++ src/main/java/RaceResultService/RaceResultsService.java
@@ -0,0 +1,4 @@
+package RaceResultService;
+
+public class RaceResultsService {
+}
---
+++ src/test/java/RaceResultServiceTest.java
@@ -0,0 +1,9 @@
+import RaceResultService.RaceResultsService;
+import org.junit.Test;
+
+public class RaceResultServiceTest {
+        @Test
+        public void subscribedClientShouldReceiveMessage() {
+            RaceResultsService raceResults = new RaceResultsService();
+        }
+    }
