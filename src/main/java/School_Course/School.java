ec5199e2d187b3d72e66d072409972b16d7d4a53
Thao <thuthaovinh@gmail.com>
2020-01-03 14:59:21 +0100

    tests added - passed

--- src/main/java/School_Course/School.java
+++ src/main/java/School_Course/School.java
@@ -2,10 +2,14 @@ package School_Course;
 
 import School_Course.Course;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class School {
-    public School(String name, String openingDate, ArrayList<Course> courses) throws IllegalArgumentException {
-
+    public School(String name, LocalDate openingDate, ArrayList<Course> courses) throws IllegalArgumentException {
+         if(name == null || name =="")
+             throw new IllegalArgumentException("Invalid School Name");
+         if(openingDate == null)
+             throw new IllegalArgumentException("Invalid Opening Date");
     }
 }
--- src/test/java/SchoolTest.java
+++ src/test/java/SchoolTest.java
@@ -2,13 +2,14 @@ import School_Course.Course;
 import School_Course.School;
 import org.junit.Test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class SchoolTest {
     private String validSchoolName = "Fontys UAS";
     private String inValidSchoolName = null;
-    private String validOpeningDate = "10-09-2019";
-    private String inValidOpeningDate = null;
+    private LocalDate validOpeningDate = LocalDate.of(2019,10,9);
+    private LocalDate inValidOpeningDate = null;
     private ArrayList<Course> courses = new ArrayList<Course>();
 
     @Test(expected = IllegalArgumentException.class)
@@ -17,12 +18,17 @@ public class SchoolTest {
     }
     @Test
     public void validSchoolNameShouldNotThrowException(){
-
+        School school = new School(validSchoolName, validOpeningDate,courses);
     }
-    @Test
+    @Test (expected = IllegalArgumentException.class)
     public void inValidOpeningDateShouldThrowException(){
-
+        School school = new School(validSchoolName, inValidOpeningDate,courses);
     }
+    @Test (expected = IllegalArgumentException.class)
+    public void inValidOpeningDateAndInvalidNameShouldThrowException(){
+        School school = new School(inValidSchoolName, inValidOpeningDate,courses);
+    }
+
     @Test
     public void addExistCourseToSchoolShouldThrowException(){
 
